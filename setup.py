b25fec1b99d726f267e861507678b35f31f12f7d
Avnish Singh <avnish1708@gmail.com>
2025-06-27 14:43:12 +0530

    1st simple update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="rain_prediction",
+    version="0.1",
+    author="avnishs17",
+    packages=find_packages(),
+    install_requires = requirements,
+)
