8527cc1e870d74dcddaa56fedef7a5c7dd7ee445
Avnish Singh <avnish1708@gmail.com>
2025-06-27 14:51:13 +0530

    data processing

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -1,17 +1,17 @@
 import sys
 
-def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
-    return error_message
-
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    def __init__(self, message: str, error_detail: Exception = None):
+        self.error_message = self.get_detailed_error_message(message, error_detail)
+        super().__init__(self.error_message)
+
+    @staticmethod
+    def get_detailed_error_message(message, error_detail):
+        """Format the error message with traceback details."""
+        _, _, exc_tb = sys.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename if exc_tb else "Unknown File"
+        line_number = exc_tb.tb_lineno if exc_tb else "Unknown Line"
+        return f"{message} | Error: {error_detail} | File: {file_name} | Line: {line_number}"
 
     def __str__(self):
         return self.error_message
--- src/logger.py
+++ src/logger.py
@@ -2,14 +2,18 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+LOGS_DIR = "logs"
+os.makedirs(LOGS_DIR,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOG_FILE = os.path.join(LOGS_DIR, f"log_{datetime.now().strftime('%Y-%m-%d')}.log")
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    filename=LOG_FILE,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    return logger
